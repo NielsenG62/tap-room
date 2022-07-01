5b0085019d3139da0ec763e58a0998140a831d7f
Gabe Nielsen <gabenielsen7@gmail.com>
2022-07-01 09:24:24 -0700

    add code to Keg file

---
+++ src/components/Keg.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Keg(props) {
+  return (
+    <React.Fragment>
+      <h2>{props.kegName}</h2>
+      <h3>{props.kegBrand}</h3>
+      <p>
+        {props.kegType} | {props.kegIBU} IBU | {props.kegAlcContent}% ABV
+      </p>
+      <p>${props.kegPrice}</p>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+Keg.propTypes = {
+  kegName: PropTypes.string,
+  kegBrand: PropTypes.string,
+  kegType: PropTypes.string,
+  kegIBU: PropTypes.integer,
+  kegAlcContent: PropTypes.integer,
+  kegPrice: PropTypes.integer,
+};
