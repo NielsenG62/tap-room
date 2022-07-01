733b37c2cc4367e20304369b5a4c88490e6686d8
Gabe Nielsen <gabenielsen7@gmail.com>
2022-07-01 10:21:27 -0700

    add code to KegList file

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,3 +1,29 @@
-// import React from "react";
-// import Keg from "./Keg";
-// import PropTypes from 'prop-types'
+import React from "react";
+import Keg from "./Keg";
+import PropTypes from "prop-types";
+
+function KegList(props) {
+  return (
+    <React.Fragment>
+      <hr />
+      {props.kegList.map((keg) => (
+        <Keg
+          whenListClicked={props.onListSelection}
+          kegName={keg.kegName}
+          kegBrand={keg.kegBrand}
+          kegType={keg.kegType}
+          kegIBU={keg.kegIBU}
+          kegAlcContent={keg.kegAlcContent}
+          kegPrice={keg.kegPrice}
+          id={keg.id}
+          key={keg.id}
+        />
+      ))}
+    </React.Fragment>
+  );
+}
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func,
+};
